4a07e507799e1312e7489a8d4d51a86264461dce
John Siegel <jpsiegel8@gmail.com>
2023-09-15 23:49:04 -0400

    UI

--- src/app/components/ScrollingText.tsx
+++ src/app/components/ScrollingText.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import React from "react";
-import { Row } from "react-bootstrap";
 import TextTransition, { presets } from "react-text-transition";
+import Spacer from "./Spacer";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 const TEXTS = ["SCIENCE", "ECONOMICS", "ALGEBRA", "SPANISH"];
 
@@ -10,18 +11,32 @@ export function ScrollingText() {
   const [index, setIndex] = React.useState(0);
 
   React.useEffect(() => {
-    const intervalId = setInterval(() => setIndex((index) => index + 1), 2000);
+    const intervalId = setInterval(() => setIndex((index) => index + 1), 1000);
     return () => clearTimeout(intervalId);
   }, []);
 
   return (
-    <Row>
-      <h1>TEACH ME ABOUT</h1>
-      <h1>
-        <TextTransition springConfig={presets.wobbly}>
-          {TEXTS[index % TEXTS.length]}
-        </TextTransition>
-      </h1>
-    </Row>
+    <div
+      style={{
+        fontSize: "48px",
+        fontFamily: "Storytime",
+        width: "100%",
+        height: "100%",
+        textAlign: "left",
+        display: "flex",
+      }}
+    >
+      <RoughNotation type={"underline"} strokeWidth={5} show={index >= 1}>
+        <h1>TEACH ME ABOUT</h1>
+      </RoughNotation>
+      <Spacer x={1} />
+      <RoughNotation type={"box"} show={true}>
+        <div style={{ width: "200px" }}>
+          <TextTransition springConfig={presets.gentle}>
+            <h1>{TEXTS[(index / 2) % TEXTS.length]}</h1>
+          </TextTransition>
+        </div>
+      </RoughNotation>
+    </div>
   );
 }
