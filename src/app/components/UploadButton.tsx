9d889463a163c430b2e0ae10ed14932717faa38d
lukedigiovanna <lukedigiovanna@gmail.com>
2023-09-16 12:57:48 -0400

    icon toolbar

--- src/app/components/UploadButton.tsx
+++ src/app/components/UploadButton.tsx
@@ -5,39 +5,39 @@ import { ChangeEvent, useRef } from "react";
 function UploadButton(props: { onUpload: (uri: string) => void }) {
   const fileUploadRef = useRef<HTMLInputElement>(null);
 
-  return (
-    <>
-      <input
-        type="file"
-        accept=".png,.jpg"
-        ref={fileUploadRef}
-        style={{ display: "none" }}
-        onChange={(e: ChangeEvent<HTMLInputElement>) => {
-          const reader = new FileReader();
-          reader.onload = (ev: ProgressEvent<FileReader>) => {
-            const img = ev.target?.result;
-            if (img) {
-              props.onUpload(img as string);
-            } else {
-              console.error("Error uploading image");
-            }
-          };
-          const files = fileUploadRef.current?.files;
-          if (files && files.length > 0) {
-            reader.readAsDataURL(files[0]);
-          }
-        }}
-      />
-      <button
-        className="bg-slate-200 hover:bg-slate-400 transition active:bg-slate-300 p-2 text-slate-800 font-bold rounded"
-        onClick={() => {
-          fileUploadRef.current?.click();
-        }}
-      >
-        Upload Image
-      </button>
-    </>
-  );
-}
+    return (
+        <>
+            <input 
+                type="file" 
+                accept=".png,.jpg" 
+                ref={fileUploadRef} 
+                style={{display: "none"}}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                    const reader = new FileReader();
+                    reader.onload = (ev: ProgressEvent<FileReader>) => {
+                        const img = ev.target?.result;
+                        if (img) {
+                            props.onUpload(img as string);
+                        }
+                        else {
+                            console.error("Error uploading image");
+                        }
+                    }
+                    const files = fileUploadRef.current?.files;
+                    if (files && files.length > 0) {
+                        reader.readAsDataURL(files[0]);
+                    }
+                }}
+            />
+            <button 
+                className="flex flex-row bg-slate-200 hover:bg-slate-400 transition active:bg-slate-300 p-1 text-slate-800 font-bold rounded text-sm" onClick={() => {
+                    fileUploadRef.current?.click()
+                }
+            }> 
+                <img src="/upload.png" className="w-4"/> <span>Upload Image</span> 
+            </button>
+        </>
+    )
+    }
 
 export { UploadButton };
--- src/app/pages/form/page.tsx
+++ src/app/pages/form/page.tsx
@@ -8,22 +8,31 @@ export default function FormPage() {
   const [imageURI, setImageURI] = useState("");
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>
-        Teach me 
-      </p>
-      <input type="text" />
-
-      <p>
-        Using
-      </p>
-      <UploadButton onUpload={(uri: string) => {
-        console.log(uri);
-        setImageURI(uri);
-      }}/>
+    <main className="flex min-h-screen items-center flex-col">
+      <div className="text-lg">
+        <div className="mb-5">
+          <span className="mr-5">
+            Teach me 
+          </span>
+        <input type="text" className="text-slate-900 p-1 text-sm rounded"/>
+        </div>
+        <div>
+          <span className="mr-5">
+            Using
+          </span>
+          <UploadButton onUpload={(uri: string) => {
+            console.log(uri);
+            setImageURI(uri);
+          }} />
+        </div>
+      </div>
       <TexturePainter backgroundImageURI={imageURI} />
 
-      <button>
+      <button onClick={() => {
+        /**
+         * Send to astica and whatever using imageURI
+         */
+      }}>
         Submit Thingy
       </button>
     </main>
