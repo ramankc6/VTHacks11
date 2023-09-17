a18f76c64a17feba2d25ea2980f36d9ada3de323
lukedigiovanna <lukedigiovanna@gmail.com>
2023-09-17 04:28:28 -0400

    visual glitch fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,26 +6,19 @@ import { Forest } from "./components/Forest";
 import { Goodwin } from "./components/Goodwin";
 import { MainPage } from "./components/Main";
 import { Loader } from "./components/Loader";
-import axios from "axios";
 
 export default function Home() {
   const [opacity, setOpacity] = useState(1.0);
   const [formOpacity, setFormOpacity] = useState(0.0);
   const [forestVisible, setForestVisible] = useState(true);
 
-  useEffect(() => {
-    (async () => {
-      const result = await axios.post("/api/narration", { text: "Hello, world "});
-      const audioUrl = result.data;
-      const audio = new Audio(audioUrl);
-      audio.play();
-    })();
-  }, [])
-
   return (
-    <div style={{
+    <span style={{
       pointerEvents: "none"
     }}>
+    {/* <div style={{
+      pointerEvents: "none"
+    }}> */}
       <Suspense fallback={<Loader />}>
         <Canvas camera={{ far: 1200 }}>
           <ambientLight intensity={0.5} />
@@ -42,6 +35,6 @@ export default function Home() {
         </Canvas>
         <MainPage opacity={opacity} formOpacity={formOpacity} />
       </Suspense>
-    </div>
+    </span>
   );
 }
